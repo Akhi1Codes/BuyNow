f5df27749a583a7e74e699d43289e7291af71cb5
Ar-Akhil <ar.akhil1511@gmail.com>
2024-11-28 14:15:15 +0530

    Feat : added react icons
    added add to shoping cart icon and removed the original price feature

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,38 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MdAddShoppingCart } from "react-icons/md";
+import { IconContext } from "react-icons/lib";
 
 const Cards = ({ name, price, id, image, seller }) => {
-  const randomNumber = Math.round((Math.random() + Number.EPSILON) * 70) + 1;
   return (
-    <div>
-      <div className="w-[170px] bg-[#0d0e14] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <Link to={`product/${id}`}>
-          <img
-            src={image[0].url}
-            alt="Product"
-            className="h-36 w-72 object-cover rounded-t-xl"
-          />
-          <div className="px-4 py-3 w-[170px]">
-            <p className="text-gray-400 mr-3 uppercase text-xs truncate">
-              {seller}
-            </p>
-            <p className="text-lg font-bold text-white truncate block capitalize">
-              {name}
-            </p>
-            <div className="flex items-center">
-              <p className="text-lg font-semibold text-white cursor-auto my-3">
-                ${price}
+    <IconContext.Provider value={{ color: "white", size: "1.2em " }}>
+      <div>
+        <div className="w-[200px] bg-[#0d0e14] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <Link to={`product/${id}`}>
+            <img
+              src={image[0].url}
+              alt="Product"
+              className="h-36 w-72 object-cover rounded-t-xl"
+            />
+            <div className="px-4 py-3 ">
+              <p className="text-gray-400 mr-3 uppercase text-xs truncate">
+                {seller}
               </p>
-              <del>
-                <p className="text-sm text-gray-600 cursor-auto ml-2">
-                  ${price + randomNumber.toFixed(2)}
+              <p className="text-lg font-bold text-white truncate block capitalize">
+                {name}
+              </p>
+              <div className="flex justify-between items-center">
+                <p className="text-lg font-semibold text-white cursor-auto my-3">
+                  ${price}
                 </p>
-              </del>
+                <MdAddShoppingCart />
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
-    </div>
+    </IconContext.Provider>
   );
 };
 
