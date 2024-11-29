f44b3ecb8082971a92d625eb6423b722a8cd14d0
Ar-Akhil <ar.akhil1511@gmail.com>
2024-11-29 09:40:18 +0530

    Fix : Config changes for icons

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-icons": "react-icons",
-    },
-  },
   build: {
     sourcemap: true,
-    rollupOptions: {
-      external: ["react-icons"],
-    },
   },
 });
