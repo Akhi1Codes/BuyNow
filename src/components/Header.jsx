491e93f403d9c2f40c5e86c8a2a3ba1cc2cab0dd
Ar-Akhil <ar.akhil1511@gmail.com>
2024-11-30 12:52:39 +0530

    updated navigation ux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,18 @@
 import { Link } from "react-router-dom";
+import HeaderNav from "./HeaderNav";
 import { useSelector, useDispatch } from "react-redux";
 import { userLogout } from "../redux/authSlice";
-import {
-  searchProducts,
-  filterProducts,
-  getAllProducts,
-} from "../redux/productSlice";
-import { useState } from "react";
+import { searchProducts } from "../redux/productSlice";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { CgComponents, CgGames } from "react-icons/cg";
-import {
-  MdElectricalServices,
-  MdOutlineFastfood,
-  MdBusinessCenter,
-  MdFitnessCenter,
-  MdOutlineSports,
-  MdShoppingCart,
-} from "react-icons/md";
-import { ImBooks } from "react-icons/im";
-import { GiClothes, GiAutoRepair } from "react-icons/gi";
-import { FaKitchenSet } from "react-icons/fa6";
-import { RiBrushFill } from "react-icons/ri";
+
+import { MdShoppingCart, MdOutlineSearch } from "react-icons/md";
 import { IconContext } from "react-icons/lib";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const searchRef = useRef(null);
   const { isAuthenticated, data } = useSelector((state) => state.auth);
   const [toggle, setToggle] = useState(false);
   const [keyword, setKeyword] = useState("");
@@ -34,49 +21,77 @@ const Header = () => {
     dispatch(searchProducts(keyword));
   };
 
+  const handleopenSearch = () => {
+    const searchBar = searchRef.current;
+    searchBar.classList.toggle("hidden");
+  };
+
   const logOut = () => {
     dispatch(userLogout());
     navigate("/");
   };
 
   return (
-    <IconContext.Provider value={{ color: "white", size: "2.5em " }}>
+    <IconContext.Provider value={{ color: "white", size: "1.5em " }}>
       <div className="sticky top-0 z-50 bg-[#0d0e14]">
         <div className="flex justify-center">
-          <nav className="  grid grid-cols-[1fr_min-content_max-content] gap-3  grid-rows-2 p-3 w-full md:flex md:w-[90%]">
-            <Link to="/">
-              <h1 className="text-3xl font-mono font-bold pr-4 text-white col-start-3">
-                BuyNow
-              </h1>
-            </Link>
-            <div className=" flex row-start-2 col-span-3 md:col-start-2 grow">
-              <input
-                type="text"
-                placeholder="Search for products..."
-                className="p-1 rounded-md grow "
-                value={keyword}
-                onChange={(e) => setKeyword(e.target.value)}
-              ></input>
-              <button onClick={handleSearch}>Search</button>
+          <nav className="p-3 flex w-full  md:w-[90%] gap-2 md:gap-3 justify-between flex-col">
+            <div className="flex justify-between md:w-full md:gap-1">
+              <Link to="/">
+                <h1 className="text-3xl font-mono font-bold pr-4 text-white mr-auto">
+                  BuyNow
+                </h1>
+              </Link>
+              <div className="flex md:w-full md:gap-3">
+                <div className="hidden md:flex m-0 grow ">
+                  <input
+                    type="text"
+                    placeholder="Search for products..."
+                    className="p-1 rounded-md grow"
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                  ></input>
+                  <button onClick={handleSearch}>Search</button>
+                </div>
+                <div className="flex gap-2 items-center ">
+                  <div className="flex items-center">
+                    <MdShoppingCart />
+                    <p className="bg-[#f2cc8f] px-2 rounded-sm text-black flex font-semibold">
+                      0
+                    </p>
+                  </div>
+                  <div className="md:hidden" onClick={handleopenSearch}>
+                    <MdOutlineSearch />
+                  </div>
+                </div>
+                <div className="px-2 flex justify-center items-center ">
+                  {isAuthenticated && data?.success ? (
+                    <img
+                      width={35}
+                      className="rounded-full h-[35px]"
+                      src={data?.user.avatar.url}
+                      alt={data?.user.name}
+                      onClick={() => setToggle(!toggle)}
+                    />
+                  ) : (
+                    <Link to="/login">
+                      <button className="text-white">Login</button>
+                    </Link>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className="flex px-2 items-center ">
-              <h6 className="px-1">Cart</h6>
-              <p className="bg-[#f2cc8f] px-2 rounded-sm text-black flex">0</p>
-            </div>
-            <div className="px-2 flex justify-center items-center ">
-              {isAuthenticated && data?.success ? (
-                <img
-                  width={35}
-                  className="rounded-full h-[35px]"
-                  src={data?.user.avatar.url}
-                  alt={data?.user.name}
-                  onClick={() => setToggle(!toggle)}
-                />
-              ) : (
-                <Link to="/login">
-                  <button className="text-white">Login</button>
-                </Link>
-              )}
+            <div className="hidden md:hidden m-0 " ref={searchRef}>
+              <div className="flex">
+                <input
+                  type="text"
+                  placeholder="Search for products..."
+                  className="p-1 rounded-md grow"
+                  value={keyword}
+                  onChange={(e) => setKeyword(e.target.value)}
+                ></input>
+                <button onClick={handleSearch}>Search</button>
+              </div>
             </div>
           </nav>
           <div>
@@ -102,92 +117,7 @@ const Header = () => {
             )}
           </div>
         </div>
-        <div className="lg:flex lg:justify-center scroll p-2 flex gap-6 whitespace-nowrap overflow-x-scroll font-light text-sm">
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(getAllProducts())}
-          >
-            <CgComponents />
-            <p>All Products</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("Electronics"))}
-          >
-            <MdElectricalServices />
-            <p>Electronics</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("Food"))}
-          >
-            <MdOutlineFastfood />
-            <p>Food</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("ClothingandAccessories"))}
-          >
-            <GiClothes />
-            <p>Clothing</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("HomeandKitchen"))}
-          >
-            <FaKitchenSet />
-            <p>Kitchen</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("BeautyandPersonal Care"))}
-          >
-            <RiBrushFill />
-            <p>Beauty Care</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("SportsandOutdoors"))}
-          >
-            <MdOutlineSports />
-            <p>Sports</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("ToysandGames"))}
-          >
-            <CgGames />
-            <p>Toys & Games</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("Books"))}
-          >
-            <ImBooks />
-            <p>Books</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("Automotive"))}
-          >
-            <GiAutoRepair />
-            <p>Automotive</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("HealthandWellness"))}
-          >
-            <MdFitnessCenter />
-            <p>Fitness</p>
-          </div>
-          <div
-            className="flex flex-col items-center justify-center cursor-pointer w-20"
-            onClick={() => dispatch(filterProducts("Office Supplies"))}
-          >
-            <MdBusinessCenter />
-            <p>Office Supplies</p>
-          </div>
-        </div>
+        <HeaderNav />
       </div>
     </IconContext.Provider>
   );
