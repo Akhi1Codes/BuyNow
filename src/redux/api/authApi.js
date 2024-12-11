297937f25feb973d8da97d144d12c8c17b22123f
Ar-Akhil <ar.akhil1511@gmail.com>
2024-12-11 10:13:00 +0530

    Fix : removed .env variables

--- src/redux/api/authApi.js
+++ src/redux/api/authApi.js
@@ -1,9 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl =
-  import.meta.env.NODE_ENV === "production"
-    ? "https://buynow-backend-iasj.onrender.com" // Production URL
-    : "http://localhost:4000"; // Development URL
+const baseUrl = "https://buynow-backend-iasj.onrender.com"; // Production URL
+//  "http://localhost:4000"; // Development URL
 
 export const authApi = createApi({
   reducerPath: "auth",
