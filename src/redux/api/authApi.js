d8ee169532d7f7ae4c221a11df66c837ac840258
Ar-Akhil <ar.akhil1511@gmail.com>
2024-12-11 10:15:05 +0530

    Fix : removed .env variables

--- src/redux/api/authApi.js
+++ src/redux/api/authApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "https://buynow-backend-iasj.onrender.com"; // Production URL
+// const baseUrl = "https://buynow-backend-iasj.onrender.com"; // Production URL
 //  "http://localhost:4000"; // Development URL
 
 export const authApi = createApi({
   reducerPath: "auth",
   baseQuery: fetchBaseQuery({
-    baseUrl,
+    baseUrl: "https://buynow-backend-iasj.onrender.com",
   }),
   tagTypes: ["User"],
   endpoints: (builder) => ({
--- src/redux/api/productApi.js
+++ src/redux/api/productApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "https://buynow-backend-iasj.onrender.com"; // Production URL
+// const baseUrl = "https://buynow-backend-iasj.onrender.com"; // Production URL
 //  "http://localhost:4000"; // Development URL
 
 export const productApi = createApi({
   reducerPath: "products",
   baseQuery: fetchBaseQuery({
-    baseUrl,
+    baseUrl: "https://buynow-backend-iasj.onrender.com",
   }),
   endpoints: (builder) => ({
     getAllProduct: builder.query({
