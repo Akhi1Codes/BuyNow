3f8afb8db109a1403b21bb7fbcc5ee92293f0b47
Ar-Akhil <ar.akhil1511@gmail.com>
2024-12-20 12:31:51 +0530

    Fix : Removed fallback state when there are no orders

--- src/pages/order/orders.jsx
+++ src/pages/order/orders.jsx
@@ -3,68 +3,47 @@ import { useUserOrdersQuery } from "../../redux/api/orderApi";
 import Loader from "../../components/Loader";
 
 const Orders = () => {
-  const { data, isLoading, error } = useUserOrdersQuery();
+  const { data, isLoading } = useUserOrdersQuery();
 
   if (isLoading) {
     return <Loader />;
   }
 
   return (
-    <div>
-      {data?.length === 0 || "" ? (
-        <div className="flex justify-center p-2 mb-4">
-          <div className="md:w-[50%]">
-            <p className="font-bold text-3xl text-center m-4">Your Orders</p>
-            <div className="text-center py-8">
-              <p className="text-lg font-semibold">
-                You have not placed any orders
-              </p>
-              <p className="text-sm text-gray-600">
-                Oops! No orders yet—let's change that!
-              </p>
-              <Link to={"/"}>
-                <p>Let's shop!</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="flex justify-center p-2 mb-4">
-          <div className="md:w-[50%]">
-            <p className="font-bold text-3xl text-center m-4">Your Orders</p>
-            <div className="py-4">
-              {data?.orders.map((order) => (
-                <div key={order._id} className="p-4 border rounded-lg mb-2">
-                  <div className="flex justify-between">
-                    <p className="font-semibold">Order ID: {order._id}</p>
-                    <p className="p-1 text-[#f2cc8f] rounded-lg text-sm font-semibold">
-                      {order.orderStatus}
-                    </p>
+    <div className="flex justify-center p-2 mb-4">
+      <div className="md:w-[50%]">
+        <p className="font-bold text-3xl text-center m-4">Your Orders</p>
+        <div className="py-4">
+          {data?.orders.map((order) => (
+            <div key={order._id} className="p-4 border rounded-lg mb-2">
+              <div className="flex justify-between">
+                <p className="font-semibold">Order ID: {order._id}</p>
+                <p className="p-1 text-[#f2cc8f] rounded-lg text-sm font-semibold">
+                  {order.orderStatus}
+                </p>
+              </div>
+              <div className="flex justify-between items-center">
+                <div>
+                  <div className="my-1">
+                    <p className="text-sm">Items:</p>
+                    {order?.orderItems.map((item) => (
+                      <p key={item._id} className="text-xs font-light pl-2">
+                        {item.name}
+                      </p>
+                    ))}
                   </div>
-                  <div className="flex justify-between items-center">
-                    <div>
-                      <div className="my-1">
-                        <p className="text-sm">Items:</p>
-                        {order?.orderItems.map((item) => (
-                          <p key={item._id} className="text-xs font-light pl-2">
-                            {item.name}
-                          </p>
-                        ))}
-                      </div>
-                      <p className="font-bold">${order.totalPrice}</p>
-                    </div>
-                    <Link to={`/order-details/${order._id}`}>
-                      <button className="bg-[#f2cc8f] text-black rounded-md mt-auto">
-                        View Details
-                      </button>
-                    </Link>
-                  </div>
+                  <p className="font-bold">${order.totalPrice}</p>
                 </div>
-              ))}
+                <Link to={`/order-details/${order._id}`}>
+                  <button className="bg-[#f2cc8f] text-black rounded-md mt-auto">
+                    View Details
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      )}
+      </div>
     </div>
   );
 };
